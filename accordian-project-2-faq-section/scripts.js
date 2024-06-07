653afa1954866a98938bbfc3f088848b97a5169c
Tek Raj Bhatt <tekbhatt1@gmail.com>
2024-06-07 11:50:56 +0545

    Accordian project for FAQ section using HTML, CSS and JavaScript
    Co-authored-by: karan chaudhary <karanchy95@gmail.com>
    Co-authored-by: Azen Moktan <azenmoktan@gmail.com>
    
    Co-authored-by: Sujan Shrestha <sujanshrestha.net@gmail.com>
    Co-authored-by: Tek Raj Bhatt <tekbhatt1@gmail.com>

---
+++ accordian-project-2-faq-section/scripts.js
@@ -0,0 +1,10 @@
+const accordion = document.querySelectorAll(".accordion");
+accordion.forEach((accordion) => {
+  const icon = accordion.querySelector(".icon");
+  const answer = accordion.querySelector(".answer");
+
+  accordion.addEventListener("click", () => {
+    icon.classList.toggle("active");
+    answer.classList.toggle("active");
+  });
+});
